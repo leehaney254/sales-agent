c801e111059e17c8505d3272e6d6e49ab3c0067c
leehaney254 <georgeleehaney20@gmail.com>
2024-05-24 11:04:25 +0300

    Docs: Add the school page

---
+++ src/app/pages/school/school.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-school',
+  standalone: true,
+  imports: [],
+  templateUrl: './school.component.html',
+  styleUrl: './school.component.css'
+})
+export class SchoolComponent {
+
+}
