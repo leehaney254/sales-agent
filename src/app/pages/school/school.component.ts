9506472f149bf5ee10fa6fe790198224d296a902
leehaney254 <georgeleehaney20@gmail.com>
2024-05-24 11:20:49 +0300

    Feat: better dashoard sidebar

--- src/app/pages/school/school.component.ts
+++ src/app/pages/school/school.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-school',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './school.component.html',
-  styleUrl: './school.component.css'
+  styleUrl: './school.component.css',
 })
-export class SchoolComponent {
-
-}
+export class SchoolComponent {}
