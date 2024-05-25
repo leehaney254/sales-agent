9250989b84266a81815848b42a3f93c89f4db9c9
leehaney254 <georgeleehaney20@gmail.com>
2024-05-25 12:44:04 +0300

    Docs: create pie chart files

---
+++ src/app/components/pirchart/pirchart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pirchart',
+  standalone: true,
+  imports: [],
+  templateUrl: './pirchart.component.html',
+  styleUrl: './pirchart.component.css'
+})
+export class PirchartComponent {
+
+}
